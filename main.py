1970210af728d194595ce1c865a7474c2197fab5
ohharper <oharper@uwo.ca>
2024-10-10 10:00:59 -0400

    Calculator project

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-sy= int(input ("start_year:"))
-ey= int(input ("end_year:"))
-age= ey - sy
-print("Age:", age)
+a= int(input("first_number:"))
+b = int(input("second_number:"))
+ 
+o="Sum:"
+print (o, a + b)
+ 
+n="Subtraction:"
+print (n, a - b)
+ 
+d="Multiplication:"
+print (d, a * b)
+ 
+r="Division:"
+print (r, a / b)
+ 
